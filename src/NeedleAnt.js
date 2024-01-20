ab2b1ba29637e51e176b2285dce20ae4d12f9901
Hares Faiez <haresfaiez@users.noreply.github.com>
2024-01-20 10:27:20 +0000

    Use Acorn to compare expressions

--- src/NeedleAnt.js
+++ src/NeedleAnt.js
@@ -1,6 +1,27 @@
-class NeedleAnt{
-  entropy(initialCode, updatedCode) {
-    return initialCode !== updatedCode ? (initialCode === 'var a' ? 4.7 : 9.4) : 0
+import * as acorn from 'acorn'
+
+class NeedleAnt {
+
+  constructor() {
+  }
+
+  coverEntropy(initialCode, updatedCode) {
+    if (initialCode === updatedCode)
+      return 0
+
+    const initialAst = acorn.parse(initialCode, { ecmaVersion: 2023 })
+
+    const updatedAst = acorn.parse(updatedCode, { ecmaVersion: 2023 })
+
+    const declaration = { initial: initialAst.body[0], updated: updatedAst.body[0] }
+
+    if (declaration.initial.kind !== declaration.updated.kind)
+      return 6
+
+    if (declaration.initial.declarations[0].id.name !== declaration.updated.declarations[0].id.name)
+      return 4
+
+    return 0
   }
 }
 
--- src/NeedleAnt.spec.js
+++ src/NeedleAnt.spec.js
@@ -1,33 +1,52 @@
 import NeedleAnt from './NeedleAnt.js'
 
 describe('Change entropy', () => {
-  it('is 0 if the string is empty both before and after', () => {
+  it('equals 0 if the string is empty both before and after', () => {
     const ant = new NeedleAnt()
-    expect(ant.entropy('', '')).toBe(0)
+    expect(ant.coverEntropy('', '')).toBe(0)
   })
 
-  it('is 0 if the string does not change', () => {
+  it('equals 0 if the string does not change', () => {
     const ant = new NeedleAnt()
-    expect(ant.entropy('a', 'a')).toBe(0)
+    expect(ant.coverEntropy('a', 'a')).toBe(0)
   })
 
-  it('is NAME_UNIT when a constant name changes', () => {
-    const initialCode = 'var a'
+  it('equals 0 when a "let" expression does not change', () => {
+    const initialCode = 'let a'
+    const updatedCode = 'let a'
+    const ant = new NeedleAnt()
+    expect(ant.coverEntropy(initialCode, updatedCode)).toBe(0)
+  })
+
+  it('equals 0 when a "let" expression does not change but the code changes', () => {
+    const initialCode = 'let a'
+    const updatedCode = 'let a;'
+    const ant = new NeedleAnt()
+    expect(ant.coverEntropy(initialCode, updatedCode)).toBe(0)
+  })
+
+  it('equals one specifier change unit when a "let" changes into a "var"', () => {
+    const initialCode = 'let a'
     const updatedCode = 'var b'
     const ant = new NeedleAnt()
-    expect(ant.entropy(initialCode, updatedCode)).toBeCloseTo(4.7, 2)
+    expect(ant.coverEntropy(initialCode, updatedCode)).toBe(6)
   })
 
-  it('is NAME_UNIT when a constant name and length change', () => {
-    const initialCode = 'var ab'
-    const updatedCode = 'var bf'
+  it('equals one identifier change unit when a constant name changes', () => {
+    const initialCode = 'const a = 1'
+    const updatedCode = 'const b = 1'
     const ant = new NeedleAnt()
-    expect(ant.entropy(initialCode, updatedCode)).toBeCloseTo(9.4, 2)
+    expect(ant.coverEntropy(initialCode, updatedCode)).toBe(4)
+  })
 
+  it('equals one identifier change unit when a constant name and length change', () => {
+    const initialCode = 'const ab = 1'
+    const updatedCode = 'const bf = 1'
+    const ant = new NeedleAnt()
+    expect(ant.coverEntropy(initialCode, updatedCode)).toBe(4)
   })
 
   // constant name change but all others stay the same
-  // var ab, var ab
 
   // a, a
   // ab, ac
@@ -35,4 +54,6 @@ describe('Change entropy', () => {
   // var/const
   // let/var
   // const a = 5, const a = 'Hello'
+  // const -> let
+  // let -> const
 })
