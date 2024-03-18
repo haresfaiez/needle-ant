6a51ca1adebbb79ea89974af8dd0f40fe5f05da2
Hares Faiez <haresfaiez@users.noreply.github.com>
2024-03-18 20:38:53 +0000

    Remove commented tests

--- src/Reflexion.spec.js
+++ src/Reflexion.spec.js
@@ -132,35 +132,3 @@ describe('Dependency entropy', () => {
     expect(entropy.calculate()).toEqual(0)
   })
 })
-
-// describe('Dependencies entropy', () => {
-// it('equals 0 when an imported file changes', () => {
-//   const initialCode = 'import A from "./a"'
-//   const updatedCode = 'import B from "./b"'
-//   const ant = new NeedleAnt(initialCode)
-//   expect(ant.coverEntropy(updatedCode)).toBe(0)
-// })
-
-// it('equals new file unit when an import stays but a new file is added in the file system', () => {
-//   const initialCode = 'import A from "./a"'
-//   const updatedCode = 'import B from "./b"'
-//   const ant = new NeedleAnt(initialCode)
-//   ant.notice('./c']
-//   expect(ant.coverEntropy(updatedCode)).toBe(100)
-// })
-
-// it('favours change that adds less dependencies', () => {
-//   const initialCode = ''
-//   const firstUpdatedCode = `
-//     import A from './a';
-//     import B from './b';
-//   `
-//   const secondUpdatedCode = 'import C from "./c"'
-
-//   const ant = new NeedleAnt(initialCode)
-//   const firstEntropy = ant.coverEntropy(firstUpdatedCode)
-//   const secondEntropy = ant.coverEntropy(secondUpdatedCode)
-
-//   expect(firstEntropy).toBeGreaterThan(secondEntropy)
-// })
-// })
