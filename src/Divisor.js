53df930cb9020c160c0a4dfa98983ca6233b9240
Hares Faiez <haresfaiez@users.noreply.github.com>
2024-03-31 19:45:03 +0000

    Simplify Divisor

--- src/Divisor.js
+++ src/Divisor.js
@@ -1,43 +1,27 @@
 export class Divisor {
   constructor(rawDivisor) {
-
-    const _divisor = rawDivisor?.dividend ? rawDivisor.divisor._divisor : rawDivisor
-
-    this._divisor = _divisor
+    this._divisor = rawDivisor?.dividend ? rawDivisor.divisor._divisor : rawDivisor
     const source = this._divisor?.odds ? this._divisor.odds() : this._divisor
-    const importedModules = this._divisor?.importedModuleExports
-    const otherModules = this._divisor?.otherModules
 
-    this.source = source
-    this.importedModules = importedModules
-    this.otherModules = otherModules
 
     this._identifiers = new Set()
-    if (Array.isArray(_divisor)) {
-      _divisor.forEach(e => this._identifiers.add(e))
+    if (Array.isArray(this._divisor)) {
+      this._divisor.forEach(e => this._identifiers.add(e))
     }
+    (source || []).forEach(e => this._identifiers.add(e))
+
+
+    this.importedModules = this._divisor?.importedModuleExports
+    this.otherModules = this._divisor?.otherModules
   }
 
   shouldFocusOnCurrentModule() {
     return Array.isArray(this._divisor)
   }
 
-  shouldCheckAdjacentModules() {
-    return !!this.otherModules
-  }
-
-  // TODO: Rename this
-  adjacentModules() {
-    return this.otherModules
-  }
-
-  importedModulesNames() {
-    return this.importedModules
-  }
-
   merge(anEntropy) {
-    const __identifiers = Array.isArray(anEntropy.delegate.divisor._divisor) ? anEntropy.delegate.divisor._divisor : anEntropy.delegate.divisor._divisor.identifiers()
-    __identifiers.forEach(eachDivisor => this._identifiers.add(eachDivisor))
+    const otherDivisor = anEntropy.delegate.divisor
+    otherDivisor.identifiers().forEach(eachDivisor => this._identifiers.add(eachDivisor))
   }
 
   addDefinitions(anEntropy) {
@@ -49,6 +33,19 @@ export class Divisor {
   }
 
   identifiersCount() {
-    return this.source.length
+    return this.identifiers().length
+  }
+
+  importedModulesNames() {
+    return this.importedModules
+  }
+
+  // TODO: Rename this
+  adjacentModules() {
+    return this.otherModules
+  }
+
+  shouldCheckAdjacentModules() {
+    return !!this.otherModules
   }
 }
