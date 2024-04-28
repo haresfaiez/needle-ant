c759ec97d931ead6f99857b0731e1e056d5ebaed
Hares Faiez <haresfaiez@users.noreply.github.com>
2024-04-28 19:24:41 +0000

    Add Evaluation custom matcher

---
+++ src/spec.js
@@ -0,0 +1,29 @@
+import { Evaluation } from './Evalution.js'
+
+beforeAll(() => {
+  jasmine.addMatchers({
+    toEvaluateTo: function() {
+      return {
+        compare: function(actual, expected) {
+          const actualLength = actual.evaluations?.length
+          if(!actualLength || (actualLength !== expected.evaluations?.length)) {
+            expect(actual).toEqual(expected)
+            return { pass: true }
+          }
+
+          actual.evaluations.forEach((eachActual, i) => {
+            const eachExpected = expected.evaluations[i]
+            if (!eachExpected.source) {
+              expect(Object.assign(new Evaluation(), eachActual, { source: undefined}))
+                .toEqual(eachExpected)
+            } else {
+              expect(eachActual).toEqual(eachExpected)
+            }
+          })
+
+          return { pass: true }
+        }
+      }
+    }
+  })
+})
