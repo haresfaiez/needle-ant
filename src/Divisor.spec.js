505da0426d29884a159c97fcdf19c71f604d56ee
Hares Faiez <haresfaiez@users.noreply.github.com>
2024-06-01 10:52:35 +0000

    Remove unneeded tests

--- src/Divisor.spec.js
+++ src/Divisor.spec.js
@@ -5,16 +5,4 @@ describe('Divisor identifiers lookup', () => {
     const actual = Divisor.parse('f.y()', (ast) => ast.body)
     expect(actual.identifiers()).toEqual(['f'])
   })
-
-  // TODO: Uncomment/delete these
-  // it('extracts called object property', () => {
-  //   const actual = Divisor.parse('f.y()', (ast) => ast.body)
-  //   expect(actual.accesses).toEqual(new Set(['y']))
-  // })
-
-  // it('extracts many objects called properties', () => {
-  //   const actual = Divisor.parse('f.y(); o.x()', (ast) => ast.body)
-
-  //   expect(actual.accesses).toEqual(new Set(['y', 'x']))
-  // })
 })
