359670d09bcabf9dddd31fc5ce6026c6a919931d
Faiez Elhares <faiez.elhares@joingsg.com>
2024-06-15 18:29:35 +0200

    Fix and improve messages of toEvaluateTo matcher

--- src/ToEvaluateTo.matcher.spec.js
+++ src/ToEvaluateTo.matcher.spec.js
@@ -1,5 +1,15 @@
 import { Evaluations } from './Evalution.js'
 
+const failedComparisonMessage = (actual, expected, failingComparisonIndex) => {
+  return `Evaluations are not the same (Comparison failed at evaluation with index: ${failingComparisonIndex}).
+\n
+  - expected:
+    ${expected.evaluations.map(JSON.stringify).join(',\n    ')}
+\n
+  - got:
+    ${actual.evaluations.map(JSON.stringify).join(',\n    ')}`
+}
+
 beforeAll(() => {
   jasmine.addMatchers({
     toEvaluateTo: function(matchersUtil) {
@@ -7,7 +17,7 @@ beforeAll(() => {
         compare: function(actual, expected) {
           const result = {}
           const isEvaluations = actual instanceof Evaluations
-          if(!isEvaluations || (actual.evaluations.length !== expected.evaluations.length)) {
+          if(!isEvaluations) {
             if (!expected.source) {
               result.pass =
                 matchersUtil.equals(actual.actual, expected.actual)
@@ -18,7 +28,18 @@ beforeAll(() => {
             return result
           }
 
-          actual.evaluations.find((eachActual, i) => {
+          if(actual.evaluations.length !== expected.evaluations.length) {
+            result.message = `Evaluations length is not the same.
+  - expected: ${expected.evaluations.length}
+  - got: ${actual.evaluations.length}
+\n\n
+${failedComparisonMessage(actual, expected)}`
+
+            result.pass = matchersUtil.equals(actual.evaluations.length, expected.evaluations.length)
+            return result
+          }
+
+          const failingComparisonIndex = actual.evaluations.find((eachActual, i) => {
             const eachExpected = expected.evaluations[i]
             if (!eachExpected.source) {
               result.pass =
@@ -28,8 +49,12 @@ beforeAll(() => {
               result.pass = matchersUtil.equals(eachActual, eachExpected)
             }
 
-            return !result.pass
+            return !result.pass && i
           })
+
+          if (!result.pass) {
+            result.message = failedComparisonMessage(actual, expected, failingComparisonIndex)
+          }
 
           return result
         }
