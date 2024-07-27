ca3849ae4948aa112ab8658674991f2741fa8370
Hares Faiez <haresfaiez@users.noreply.github.com>
2024-07-27 10:21:31 +0000

    Extract a module for fullMetricTraceEvents test

---
+++ spec/fixtures/fullMetricTraceEvents.js
@@ -0,0 +1,46 @@
+// BASED ON: https://github.com/GoogleChrome/lighthouse/blob/main/core/lib/traces/metric-trace-events.js
+export default {
+  code: `
+        const getUberMetrics = (x) => x;
+
+        class MetricTraceEvents {
+            gatherMetrics() {
+                const uberMetrics = getUberMetrics(this._auditResults);
+                if (!uberMetrics) {
+                    return [];
+                }
+
+                return uberMetrics;
+            }
+
+            getTimeOriginEvt(metrics, uberMetrics) {
+                const resolvedMetrics = [];
+                MetricTraceEvents.metricsDefinitions.forEach(metric => {
+                    const ts = uberMetrics[metric.tsKey];
+                    if (ts === undefined) {
+                    log.error('pwmetrics-events', ` + '`${metric.name} timestamp not found`' + `);
+                    return;
+                    }
+
+                    resolvedMetrics.push({
+                    id: metric.id,
+                    name: metric.name,
+                    ts,
+                    });
+                });
+
+                const timeOriginMetric = metrics.find(e => e.id === 'timeorigin');
+                if (!timeOriginMetric) return {errorMessage: 'timeorigin Metric not found in definitions'};
+                try {
+                    const frameIds = TraceProcessor.findMainFrameIds(this._traceEvents);
+                    return {pid: frameIds.startingPid, tid: 1, ts: timeOriginMetric.ts};
+                } catch (err) {
+                    return {errorMessage: err.message};
+                }
+            }
+        }
+    `,
+  expected: [
+  ]
+}
+  
--- src/modules.spec.js
+++ src/modules.spec.js
@@ -2,78 +2,34 @@ import lighthouse from '../spec/fixtures/lighthouse.js'
 import NeedleAnt from './NeedleAnt.js'
 import { NumericEvaluation, NullEvaluation } from './Evalution.js'
 import metricTraceEvents from '../spec/fixtures/metricTraceEvents.js'
+import fullMetricTraceEvents from '../spec/fixtures/fullMetricTraceEvents.js'
+
+const buildExpected = (expected) =>
+  expected
+    .reduce(
+      (acc, e) =>acc.plus(new NumericEvaluation(e.actual, e.possible, e.source)),
+      new NullEvaluation()
+    )
 
 describe('Module entropy', () => {
   it('calculates entropy of one function defintion', () => {
     const actual = new NeedleAnt(lighthouse.code).entropy()
 
-    const expected = lighthouse
-      .expected
-      .reduce(
-        (acc, e) =>acc.plus(new NumericEvaluation(e.actual, e.possible, e.source)),
-        new NullEvaluation()
-      )
+    const expected = buildExpected(lighthouse.expected)
     expect(actual.evaluate()).toEvaluateTo(expected)
   })
 
   it('calculates entropy of class definition', () => {
     const actual = new NeedleAnt(metricTraceEvents.code).entropy()
 
-    const expected = metricTraceEvents
-      .expected
-      .reduce(
-        (acc, e) =>acc.plus(new NumericEvaluation(e.actual, e.possible, e.source)),
-        new NullEvaluation()
-      )
+    const expected = buildExpected(metricTraceEvents.expected)
     expect(actual.evaluate()).toEvaluateTo(expected)
   })
 
-  // TODO: turn-on and fix these
-  xit('', () => {
-    const code = `
-    class MetricTraceEvents {
-        gatherMetrics() {
-        const uberMetrics = getUberMetrics(this._auditResults);
-        if (!uberMetrics) {
-            return [];
-        }
+  xit('calculates entropy of class definition with two methods', () => {
+    const actual = new NeedleAnt(fullMetricTraceEvents.code).entropy()
 
-        return uberMetrics;
-        }
-
-        getTimeOriginEvt(metrics, uberMetrics) {
-        const resolvedMetrics = [];
-        MetricTraceEvents.metricsDefinitions.forEach(metric => {
-            const ts = uberMetrics[metric.tsKey];
-            if (ts === undefined) {
-            log.error('pwmetrics-events', ` + '`${metric.name} timestamp not found`' + `);
-            return;
-            }
-
-            resolvedMetrics.push({
-            id: metric.id,
-            name: metric.name,
-            ts,
-            });
-        });
-
-        const timeOriginMetric = metrics.find(e => e.id === 'timeorigin');
-        if (!timeOriginMetric) return {errorMessage: 'timeorigin Metric not found in definitions'};
-        try {
-            const frameIds = TraceProcessor.findMainFrameIds(this._traceEvents);
-            return {pid: frameIds.startingPid, tid: 1, ts: timeOriginMetric.ts};
-        } catch (err) {
-            return {errorMessage: err.message};
-        }
-        }
-    }
-    `
-    const actual = new NeedleAnt(code).entropy()
-
-    const expected = new NumericEvaluation(2, 3)
-      .plus(new NumericEvaluation(2, 3))
-      .plus(new NumericEvaluation(1, 3))
-      .plus(new NumericEvaluation(1, 3))
+    const expected = buildExpected(fullMetricTraceEvents.expected)
     expect(actual.evaluate()).toEvaluateTo(expected)
   })
 })
