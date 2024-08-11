7d22377103a8b4a920361b625d9fd2babbf62640
Hares Faiez <haresfaiez@users.noreply.github.com>
2024-08-11 20:08:11 +0000

    Restore UI test

--- src/ui.spec.js
+++ src/ui.spec.js
@@ -1,59 +1,18 @@
-// TODO: Uncomment and fix these
-// import { IdentifiersEvaluation, NumericEvaluation } from './Evalution.js'
-// import NeedleAnt from './NeedleAnt.js'
+import { CodeBag } from './CodeBag.js'
+import { BagEvaluation, NumericEvaluation } from './Evalution.js'
+import NeedleAnt from './NeedleAnt.js'
 
-// describe('Entropy evaluation', () => {
-//   it('uses the exact literal in the actual/possibilities identifiers array', () => {
-//     const code = 'const a = 4;'
-//     const actual = new NeedleAnt(code).entropy()
- 
-//     const identifiersEvaluation = new IdentifiersEvaluation([4], ['a', 4], '4')
-//     const expected = new NumericEvaluation(1, 2, identifiersEvaluation)
-//     expect(actual.evaluate()).toEqual(expected)
-//   })
-// })ExpressionEntropyBag
-// import { IdentifiersEvaluation, NumericEvaluation, UiEvaluation } from './Evalution.js'
-// import NeedleAnt from './NeedleAnt.js'
+describe('Entropy evaluation', () => {
+  it('uses the exact literal in the actual/possibilities identifiers array', () => {
+    const actual = new NeedleAnt('const a = 4;').entropy()
 
-// TODO: Uncomment and fix these
-// describe('Entropy evaluation', () => {
-//   it('uses the exact literal in the actual/possibilities identifiers array', () => {
-//     const code = 'const a = 4;'
-//     const actual = new NeedleAnt(code).entropy().evaluate()
-
-//     const literalCodeSlice = new CodeSlice('4', 10, 11)
-//     const identifiersEvaluation = new IdentifiersEvaluation([literalCodeSlice], ['a', literalCodeSlice], '4')
-//     const expected = new NumericEvaluation(1, 2, identifiersEvaluation)
-
-//     expect(actual).toEqual(expected)
-//   })
-// })
-
-//   // TODO: Add a test to highlight also the possiblities
-//   describe('Entropy UI evaluation', () => {
-//   it('contains a literal position in code', () => {
-//     const code = 'const a = 4;'
-//     const actual = new NeedleAnt(code).entropy().draw()
-
-//     const literalCodeSlice = new CodeSlice('4', 10, 11)
-//     const identifiersEvaluation = new IdentifiersEvaluation([literalCodeSlice], ['a', literalCodeSlice], '4')
-//     const numericEvaluation = new NumericEvaluation(1, 2, identifiersEvaluation)
-//     const expected = new UiEvaluation(numericEvaluation, [literalCodeSlice])
-
-//     expect(actual).toEqual(expected)
-//   })
-
-// it('', () => {
-//   const code = 'const increment = (aNumber) => aNumber + 1; const result = increment(2);'
-//   const actual = new NeedleAnt(code).entropy()
-
-//   const expected = new
-
-//   const expected = new NumericEvaluation(1, 2)
-//     .plus(new NumericEvaluation(1, 3))
-//     .plus(new NumericEvaluation(1, 3))
-//     .plus(new NumericEvaluation(1, 2))
-//     .plus(new NumericEvaluation(1, 3))
-//   expect(actual.evaluate()).toEvaluateTo(expected)
-// })
-// })
+    const expectedActual = CodeBag.fromNodes([{ name: 4, start: 10, end: 11}])
+    const expectedPossible = CodeBag.fromNodes([
+      { name: 4, start: 10, end: 11},
+      { name: 'a', start: 6, end: 7},
+    ])
+    const bagEvaluation = new BagEvaluation(expectedActual, expectedPossible, '4')
+    const expected = new NumericEvaluation(1, 2, bagEvaluation)
+    expect(actual.evaluate()).toEqual(expected)
+  })
+})
