f15e00380693bdfe70c7c30aa0e6cdcb68daece0
Hares Faiez <haresfaiez@users.noreply.github.com>
2024-08-11 08:21:45 +0000

    Remove semicolons

--- src/CodeBag.js
+++ src/CodeBag.js
@@ -2,7 +2,7 @@
 // TODO: Move together with CodeSlice to a separate sub-folder
 export class CodeBag {
   constructor() {
-    this.elements = new Map();
+    this.elements = new Map()
   }
 
   insert(anotherCodeBag) {
@@ -14,22 +14,22 @@ export class CodeBag {
     // TODO: throw error here if codeSlices is empty, or if raw does not exists
     const id = codeSlices[0]?.raw
     const codeSlicesPerId = this.elements.get(id);
-    this.elements.set(id, [...(codeSlicesPerId || []), ...codeSlices]);
+    this.elements.set(id, [...(codeSlicesPerId || []), ...codeSlices])
   }
 
   put(codeSlice) {
     const codeSlicesPerId = this.elements.get(codeSlice.raw);
-    this.elements.set(codeSlice.raw, [...(codeSlicesPerId || []), codeSlice]);
+    this.elements.set(codeSlice.raw, [...(codeSlicesPerId || []), codeSlice])
   }
 
   collect(sources, collector) {
     for (const eachSource of sources) {
-      collector(eachSource, this);
+      collector(eachSource, this)
     }
   }
 
   // TODO: Reomve this
   evaluate() {
-    return [...this.elements.keys()];
+    return [...this.elements.keys()]
   }
 }
